1e0bfc98b00cb916c431ab67e2b4d89d608c25ba
TomCarr <t.i.w.carr@gmail.com>
2024-12-03 14:18:46 +0000

    Cleaning up Program.cs.

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -5,7 +5,7 @@ using AdventOfCode2024.Days.Day3;
 var part1Result = await Part1.Run();
 Console.WriteLine(part1Result);
 
-var part2Result = await AdventOfCode2024.Days.Day3.Part2.Run();
+var part2Result = await Part2.Run();
 Console.WriteLine(part2Result);
 
 Console.ReadLine();
